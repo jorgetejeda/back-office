2844859525381ca71c91fc96024531efb7f9b4b1
Jorge Tejeda <jorgetejeda0804@gmail.com>
2024-03-21 21:11:18 -0400

    init project

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+*,
+::before,
+::after {
+  box-sizing: inherit;
+  background-repeat: no-repeat;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  min-block-size: 100%;
+}
+
+.layout-page-content {
+  flex-grow: 1;
+  padding-block: 1.5rem;
+}
+
+.layout-footer {
+  /* .footer-content-container {
+    block-size: variables.$layout-vertical-nav-footer-height;
+  } */
+
+  .layout-footer-sticky & {
+    position: sticky;
+    inset-block-end: 0;
+    will-change: transform;
+  }
+
+  .layout-footer-hidden & {
+    display: none;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <button class="primary">Hola mundo</button>
+  </main>
+</template>
+
+<style scoped lang="scss">
+button.primary {
+  background-color: red;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 500;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: darkred;
+  }
+
+  &:active {
+    background-color: red;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: lightgray;
+    color: gray;
+    cursor: not-allowed;
+  }
+}
+</style>
